eca83e7d7440f1d3f51d95a1ef0ebb4442ea6709
kvegr <CMAKOJIbIK>
2023-12-17 11:59:49 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package testproject
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="themes/general.css">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+<section class="header">
+    <?php
+    get_template_part('page/nav/nav', 'to');
+    ?>
+</section>
+
+
+
---
+++ page/nav/nav.php
@@ -0,0 +1,21 @@
+<div class="nav">
+    <div class="img">
+        <img src="https://i.natgeofe.com/n/680eed24-fa0d-460a-88fe-1951b35f5a53/proboscis-monkey_thumb_2x3.jpg" alt="">
+    </div>
+    <h1>Hi i`m monkey. I have long nose</h1>
+    <h2>what monkey love</h2>
+    <ul class="monkey">
+        <li>banana</li>
+        <li>nuts</li>
+        <li>nudes</li>
+
+    </ul>
+    <div class="help"></div>
+    <div class="text">
+        <p>Please help monkey</p>
+        <span>just pay</span>
+    </div>
+    <div class="button">
+        <button>PAY!!!</button>
+    </div>
+</div>
